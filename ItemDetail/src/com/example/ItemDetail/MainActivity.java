d3ca135d5288e83383f0fea6cd9650fc923699a5
hint <hongye.hwy@taobao.com>
2013-05-20 07:43:06 +0800

    2ed commit

--- ItemDetail/src/com/example/ItemDetail/MainActivity.java
+++ ItemDetail/src/com/example/ItemDetail/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.ItemDetail;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    ListView itemEvaluateLst;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        itemEvaluateLst = (ListView)findViewById(R.id.item_evaluate_lst);
+//        itemEvaluateLst.setAdapter(getItemEvaluateLst());
     }
+
+    private Adapter getItemEvaluateLst(){
+        return  null;
+    }
+
 }
